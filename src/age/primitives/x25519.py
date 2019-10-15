82d2f79d3c23cd482c311dbcf2cf2c6460e1ad9c
Jonas Lieb <lieb.jonas@gmail.com>
2019-10-15 21:36:59 +0200

    Fix typing for x25519 scalarmult

--- src/age/primitives/x25519.py
+++ src/age/primitives/x25519.py
@@ -16,12 +16,12 @@ def x25519_scalarmult(secret_scalar: ECScalar, point: ECPoint) -> ECPoint:
 
     k = crypto_scalarmult(secret_scalar, point)
     assert any(k), "All-zeros-check failed (see RFC 7748, section 6.1)"
-    return k
+    return ECPoint(k)
 
 
 def x25519_scalarmult_base(scalar: ECScalar) -> ECPoint:
-    return crypto_scalarmult_base(scalar)
+    return ECPoint(crypto_scalarmult_base(scalar))
 
 
 def x25519_reduce(k: ECScalar) -> ECScalar:
-    return crypto_core_ed25519_scalar_reduce(k)
+    return ECScalar(crypto_core_ed25519_scalar_reduce(k))
